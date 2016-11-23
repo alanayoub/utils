d2376537a4793dd10f953567421ce8481099602f
Alan Ayoub <alanayoub@gmail.com>
2016-11-23 09:46:22 +0000

    Adding default and a custom template

--- build/src/apps/app-02/main.js
+++ build/src/apps/app-02/main.js
@@ -1,10 +1,8 @@
 import 'babel-polyfill';
 require.ensure([
     './views/view-01/index.js',
-    './views/view-02/index.js'
 ], function (require) { // Arrow functions webpack issue 3315
     const view01 = require('./views/view-01/index.js');
-    const view02 = require('./views/view-02/index.js');
     //
-    document.getElementsByTagName('body')[0].innerHTML += '<h1>App 01 / View 01</h1>';
+    document.getElementsByTagName('body')[0].innerHTML += '<h1>App 02 / View 01</h1>';
 });
